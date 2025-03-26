baa95260b4253b3b6dd800a9917671e45f5ea288
Axel Guerin <59256787+axelguerin@users.noreply.github.com>
2025-03-26 11:56:50 +0100

    Add files via upload

--- som.py
+++ som.py
@@ -1,5 +1,5 @@
-from random import random
-from math import sqrt
+from random import random, randint
+from math import sqrt, inf, exp
 
 def d_manhattan(a, b):
     sum = 0
@@ -18,14 +18,14 @@ def d_neuronne(xA, yA, xB, yB):
 
 class SOM():
 
-    def __init__(self, X, epsilon_0, T_max, map_size):
-        self.X = X
-        self.n = len(X[0])
+    def __init__(self, epsilon_0, T_max, map_size):
         self.epsilon_0 = epsilon_0
         self.T_max = T_max
         self.map_size = map_size
-        self.W = self.init_W()
+        self.sigma_0 = 0.5 * map_size
         self.X = self.init_X()
+        self.n = len(self.X[0])
+        self.W = self.init_W()
         
     def init_W(self):
         W = []
@@ -50,9 +50,76 @@ class SOM():
             for j in range(len(X[i])):
                 X[i][j] = float(X[i][j])
         return X
+    
+    def fit(self):
+        for t in range(self.T_max):
+            n = randint(0, len(self.X) - 1)
+
+            i_min = 0
+            j_min = 0
+            d_min = inf
+
+            for i in range(self.map_size):
+                for j in range(self.map_size):
+                    d = d_manhattan(self.X[n], self.W[i][j])
+                    if d < d_min:
+                        d_min = d
+                        i_min = i
+                        j_min = j
+            
+            sigma = self.sigma_0 * exp(-t / self.T_max)
+            epsilon = self.epsilon_0 * exp(-t / self.T_max)
+
+            for i in range(self.map_size):
+                for j in range(self.map_size):
+                    d = d_neuronne(i_min, j_min, i, j)
+                    theta = exp(- (d * d) / (2 * sigma * sigma))
+                    for k in range(self.n):
+                        self.W[i][j][k] += theta * epsilon * (self.X[n][k] - self.W[i][j][k])
+
+    def assign(self):
+        self.assignment = []
+        for t in range(len(self.X)):
+            i_min = 0
+            j_min = 0
+            d_min = inf
+
+            for i in range(self.map_size):
+                for j in range(self.map_size):
+                    d = d_manhattan(self.X[t], self.W[i][j])
+                    if d < d_min:
+                        d_min = d
+                        i_min = i
+                        j_min = j
+            self.assignment.append([i_min, j_min])
+        return self.assignment
 
 
 
-som = SOM([[1,1,1,1]], 0.01, 100, 10)
 
+som = SOM(0.01, 10000, 2)
+som.fit()
+assignment = som.assign()
 
+M = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+for t in range(len(assignment)):
+    i = 0
+    j = 0
+    if assignment[t] == [0, 0]:
+        i = 0
+    if assignment[t] == [0, 1]:
+        i = 1
+    if assignment[t] == [1, 0]:
+        i = 2
+    if assignment[t] == [1, 1]:
+        i = 3
+    if t < 50:
+        j = 0
+    elif t < 100:
+        j = 1
+    else:
+        j = 2
+    M[i][j] += 1
+
+print(M)
